37c3dae14f0144f971ca67eb45d360eadeb5ea4b
xande <aleturial8@gmail.com>
2021-04-02 17:36:50 -0300

    Card component

---
+++ nubanck_clone/lib/Pages/Home/widget/Page_view_app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:nubanck_clone/Pages/Home/widget/card_app.dart';
+
+class PageViewApp extends StatelessWidget {
+  final double top;
+  final ValueChanged<int> onChanged;
+  const PageViewApp({Key key, this.top, this.onChanged}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    
+    return Positioned(
+        top: top,
+        height: MediaQuery.of(context).size.height * .45,
+        width: MediaQuery.of(context).size.width,
+        child: SizedBox(
+          child: PageView(
+            onPageChanged: onChanged,
+            physics: BouncingScrollPhysics(), children: <Widget>[
+            CardApp(),
+            CardApp(),
+            CardApp(),
+          ]),
+      )
+    );
+  }
+}
